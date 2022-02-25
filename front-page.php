0293eb4ea30cba0c2f2e463cc87776af3430c628
Adrien Benete <adrbenete@gmail.com>
2022-02-25 17:59:45 +0900

    ajout des articles

--- front-page.php
+++ front-page.php
@@ -34,22 +34,60 @@ get_header();
                 </div>
                 <div class="carousel-item" data-bs-interval="2000">
                     <img src="<?php echo get_template_directory_uri(); ?>/img/plant2.jpg" alt="plant image">
-                    < </div>
-                        <div class="carousel-item">
-                            <img src="<?php echo get_template_directory_uri(); ?>/img/plant1.webp" alt="plant image">
-
-                        </div>
                 </div>
-                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Previous</span>
-                </button>
-                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Next</span>
-                </button>
+                <div class="carousel-item">
+                    <img src="<?php echo get_template_directory_uri(); ?>/img/plant1.webp" alt="plant image">
+
+                </div>
             </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
     </section>
+
+    <section class="container">
+        <h1 class="text-center pt-5">Popular Products</h1>
+        <p class="text-center">
+            We offer a number of healthy and beautiful plants <br> for your indoor and outdoor space.
+        </p>
+        <div class="pt-5 pb-5">
+
+            <?php echo do_shortcode(' [products colums=4 limit=4] ') ?>
+        </div>
+
+    </section>
+
+    <section class="categories pt-5 pb-5">
+        <div class="container">
+            <h1 class="text-center pt-5">Categories</h1>
+            <p class="text-center">
+                We offer a number of healthy and beautiful plants <br> for your indoor and outdoor space.
+            </p>
+
+        </div>
+
+        <div class="row pt-5">
+
+            <div class="categories__col col-md-4 col-sm-12 ">
+                <a href="#">
+                    <img src="" alt="" class="" loading="lazy">
+                </a>
+
+            </div>
+        </div>
+
+
+
+    </section>
+
+
+
 
 </main><!-- #main -->
 
